7cbe31377a52eb354b74e5a7f3a93f99f2c3aa5e
Naik <naiksav4enko@gmail.com>
2016-06-05 13:46:50 +0300

    Rename package

---
+++ library/src/main/java/ua/naiksoftware/rxgoogle/BaseSingle.java
@@ -0,0 +1,143 @@
+package ua.naiksoftware.rxgoogle;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import rx.Single;
+import rx.SingleSubscriber;
+import rx.functions.Action0;
+import rx.subscriptions.Subscriptions;
+
+/* Copyright (C) 2015 Michał Charmas (http://blog.charmas.pl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ---------------
+ *
+ * FILE MODIFIED by Patrick Löwenstein, 2016
+ *
+ */
+public abstract class BaseSingle<T> extends BaseRx<T> implements Single.OnSubscribe<T> {
+    private final boolean handleResolution;
+
+    private final HashMap<GoogleApiClient, SingleSubscriber<? super T>> subscriptionInfoHashMap = new HashMap<>();
+
+    protected BaseSingle(@NonNull RxGoogle rxFit, Long timeout, TimeUnit timeUnit) {
+        super(rxFit, timeout, timeUnit);
+        handleResolution = true;
+    }
+
+    protected BaseSingle(@NonNull Context ctx, @NonNull Api<? extends Api.ApiOptions.NotRequiredOptions>[] services, Scope[] scopes) {
+        super(ctx, services, scopes);
+        handleResolution = false;
+    }
+
+    @Override
+    public final void call(SingleSubscriber<? super T> subscriber) {
+        final GoogleApiClient apiClient = createApiClient(new ApiClientConnectionCallbacks(subscriber));
+        subscriptionInfoHashMap.put(apiClient, subscriber);
+
+        try {
+            apiClient.connect();
+        } catch (Throwable ex) {
+            subscriber.onError(ex);
+        }
+
+        subscriber.add(Subscriptions.create(new Action0() {
+            @Override
+            public void call() {
+                if (apiClient.isConnected() || apiClient.isConnecting()) {
+                    onUnsubscribed(apiClient);
+                    apiClient.disconnect();
+                }
+
+                subscriptionInfoHashMap.remove(apiClient);
+            }
+        }));
+    }
+
+    protected abstract void onGoogleApiClientReady(GoogleApiClient apiClient, SingleSubscriber<? super T> subscriber);
+
+    protected final void handleResolutionResult(int resultCode, ConnectionResult connectionResult) {
+        for (Map.Entry<GoogleApiClient, SingleSubscriber<? super T>> entry : subscriptionInfoHashMap.entrySet()) {
+            if (!entry.getValue().isUnsubscribed()) {
+                if (resultCode == Activity.RESULT_OK) {
+                    try {
+                        entry.getKey().connect();
+                    } catch (Throwable ex) {
+                        entry.getValue().onError(ex);
+                    }
+                } else {
+                    entry.getValue().onError(new GoogleAPIConnectionException("Error connecting to GoogleApiClient, resolution was not successful.", connectionResult));
+                }
+            }
+        }
+    }
+
+    protected class ApiClientConnectionCallbacks extends BaseRx.ApiClientConnectionCallbacks {
+
+        final protected SingleSubscriber<? super T> subscriber;
+
+        private GoogleApiClient apiClient;
+
+        private ApiClientConnectionCallbacks(SingleSubscriber<? super T> subscriber) {
+            this.subscriber = subscriber;
+        }
+
+        @Override
+        public void onConnected(Bundle bundle) {
+            try {
+                onGoogleApiClientReady(apiClient, subscriber);
+            } catch (Throwable ex) {
+                subscriber.onError(ex);
+            }
+        }
+
+        @Override
+        public void onConnectionSuspended(int cause) {
+            subscriber.onError(new GoogleAPIConnectionSuspendedException(cause));
+        }
+
+        @Override
+        public void onConnectionFailed(ConnectionResult connectionResult) {
+            if(handleResolution && connectionResult.hasResolution()) {
+                observableSet.add(BaseSingle.this);
+
+                if(!ResolutionActivity.isResolutionShown()) {
+                    Intent intent = new Intent(ctx, ResolutionActivity.class);
+                    intent.putExtra(ResolutionActivity.ARG_CONNECTION_RESULT, connectionResult);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    ctx.startActivity(intent);
+                }
+            } else {
+                subscriber.onError(new GoogleAPIConnectionException("Error connecting to GoogleApiClient.", connectionResult));
+            }
+        }
+
+        public void setClient(GoogleApiClient client) {
+            this.apiClient = client;
+        }
+    }
+}
---
+++ library/src/main/java/ua/naiksoftware/rxgoogle/SessionStartSingle.java
@@ -0,0 +1,38 @@
+package ua.naiksoftware.rxgoogle;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.Session;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.SingleSubscriber;
+
+/* Copyright 2016 Patrick Löwenstein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+public class SessionStartSingle extends BaseSingle<Status> {
+
+    private final Session session;
+
+    SessionStartSingle(RxGoogle rxFit, Session session, Long timeout, TimeUnit timeUnit) {
+        super(rxFit, timeout, timeUnit);
+        this.session = session;
+    }
+
+    @Override
+    protected void onGoogleApiClientReady(GoogleApiClient apiClient, final SingleSubscriber<? super Status> subscriber) {
+        setupFitnessPendingResult(Fitness.SessionsApi.startSession(apiClient, session), new StatusResultCallBack(subscriber));
+    }
+}
